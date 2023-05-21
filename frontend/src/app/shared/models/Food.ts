10d84806bae16272349c889dd3188ad72b5e89c3
MateuszStasica <stasica.mateusz18@gmail.com>
2023-05-21 12:07:30 +0200

    Food Thumbnails

---
+++ frontend/src/app/shared/models/Food.ts
@@ -0,0 +1,9 @@
+export class Food{
+  id!:string;
+  name!:string;
+  favourite!:boolean;
+  imageUrl!:string;
+  cookTime!:string;
+  ingredients!:string[];
+  recipe!:string;
+}
---
+++ frontend/src/data.ts
@@ -0,0 +1,31 @@
+import {Food} from './app/shared/models/Food';
+
+export const sample_foods: Food[] = [
+  {
+    id:'1',
+    name:'Lasagne',
+    favourite:false,
+    imageUrl:'assets/images/food1.jpg',
+    cookTime:'70-90 min',
+    ingredients:['a','b','c'],
+    recipe:'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'
+  },
+  {
+    id:'2',
+    name:'Shakshuka',
+    favourite:false,
+    imageUrl:'assets/images/food2.jpg',
+    cookTime:'20-30 min',
+    ingredients:['a','b','c'],
+    recipe:'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla'
+  },
+  {
+    id:'3',
+    name:'Spaghetti Bolognese',
+    favourite:false,
+    imageUrl:'assets/images/food3.jpg',
+    cookTime:'45-60 min',
+    ingredients:['a','b','c'],
+    recipe:'bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla',
+  }
+]
